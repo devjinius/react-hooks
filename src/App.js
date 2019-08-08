083a19bdb66c81722bffee8f5b20f51cb90a02d6
eugene94 <eugenekang94@gmail.com>
2019-08-08 15:04:56 +0900

    Feat: add user

--- src/App.js
+++ src/App.js
@@ -1,16 +1,61 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Hello from './components/Hello';
 import Counter from './components/Counter';
 import Input from './components/Input';
 import List from './components/List';
+import CreateUser from './components/CreateUser';
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.ttt@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ]);
+
+  const nextId = useRef(4);
+
+  const onChange = ({ target: { name, value } }) => {
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const onCreate = () => {
+    const newUser = { id: nextId.current, ...inputs };
+    setUsers([...users, newUser]);
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
+  };
+
   return (
     <div>
       <Hello />
       <Counter />
       <Input />
-      <List />
+      <List users={users} />
+      <CreateUser {...inputs} onChange={onChange} onCreate={onCreate} />
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -8,25 +8,7 @@ function User({ user }) {
   );
 }
 
-function List() {
-  const users = [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.ttt@gmail.com'
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com'
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com'
-    }
-  ];
-  User;
+function List({ users }) {
   const userComponents = _ => users.map(user => <User user={user} key={user.id} />);
   return userComponents();
 }
