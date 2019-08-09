ed1e4d6230679916690f8353e745fe2fed498bba
eugene94 <eugenekang94@gmail.com>
2019-08-09 14:35:45 +0900

    Feat: Count

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useMemo } from 'react';
 import Hello from './components/Hello';
 import Counter from './components/Counter';
 import Input from './components/Input';
 import List from './components/List';
 import CreateUser from './components/CreateUser';
+
+function countActiveUser(users) {
+  console.log('!!!');
+  return users.filter(user => user.active).length;
+}
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -15,17 +20,20 @@ function App() {
     {
       id: 1,
       username: 'velopert',
-      email: 'public.ttt@gmail.com'
+      email: 'public.ttt@gmail.com',
+      active: true
     },
     {
       id: 2,
       username: 'tester',
-      email: 'tester@example.com'
+      email: 'tester@example.com',
+      active: false
     },
     {
       id: 3,
       username: 'liz',
-      email: 'liz@example.com'
+      email: 'liz@example.com',
+      active: false
     }
   ]);
 
@@ -53,13 +61,20 @@ function App() {
     setUsers(users.filter(user => id !== user.id));
   };
 
+  const onToggle = id => {
+    setUsers(users.map(user => (user.id === id ? { ...user, active: !user.active } : user)));
+  };
+
+  const count = useMemo(() => countActiveUser(users), [users]);
+
   return (
     <div>
-      <Hello />
-      <Counter />
-      <Input />
-      <List users={users} onRemove={onRemove} />
+      {/* <Hello /> */}
+      {/* <Counter /> */}
+      {/* <Input /> */}
+      <List users={users} onRemove={onRemove} onToggle={onToggle} />
       <CreateUser {...inputs} onChange={onChange} onCreate={onCreate} />
+      <p>현재 활성화 계정 : {count}</p>
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function User({ user, onRemove }) {
+function User({ user, onRemove, onToggle }) {
   return (
     <div>
-      <b>{user.username}</b> <span>({user.email})</span>{' '}
-      <button onClick={() => onRemove(user.id)}>x</button>
+      <b
+        onClick={() => onToggle(user.id)}
+        style={{ cursor: 'pointer', color: user.active ? 'green' : 'black' }}
+      >
+        {user.username}
+      </b>{' '}
+      <span>({user.email})</span> <button onClick={() => onRemove(user.id)}>x</button>
     </div>
   );
 }
 
-function List({ users, onRemove }) {
+function List({ users, onRemove, onToggle }) {
   const userComponents = _ =>
-    users.map(user => <User user={user} key={user.id} onRemove={onRemove} />);
-  return userComponents();
+    users.map(user => <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle} />);
+
+  return <>{userComponents()}</>;
 }
 
 export default List;
